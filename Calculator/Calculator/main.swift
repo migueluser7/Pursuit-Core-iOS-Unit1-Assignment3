fd7c304ba77b08f2bfb828b9436ed944332427c7
miguel <pursuit@MB-pro.local>
2019-10-11 14:45:01 -0400

    fu

--- Calculator/Calculator/main.swift
+++ Calculator/Calculator/main.swift
@@ -22,3 +22,10 @@ func mathStuffFactory(opString: String) -> (Double, Double) -> Double {
     return {x, y in x + y }
   }
 }
+
+let closureOperation = mathStuffFactory(opString: "+")
+
+
+let result = closureOperation(45, 5)
+
+print("the result of the operation is \(Int(result))")
