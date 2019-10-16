5941e23ab6482bb0302ee93c156b712a1bdbd030
miguel <pursuit@MB-pro.local>
2019-10-16 17:21:17 -0400

    u1

--- Calculator/Calculator/main.swift
+++ Calculator/Calculator/main.swift
@@ -29,3 +29,27 @@ let closureOperation = mathStuffFactory(opString: "+")
 let result = closureOperation(45, 5)
 
 print("the result of the operation is \(Int(result))")
+
+
+let mathFunction = mathStuffFactory(opString: "5 + 25")
+
+let userInput = readLine() ?? "5 + 5"
+
+print(userInput.components(separatedBy: " ")) // separates string into array but looks for " " to reparate each string
+
+
+func takeInInput(input: String) -> Double {
+    
+    let userInputArray = input.components(separatedBy: " ")
+    
+    let mathFunction = mathStuffFactory(opString: userInputArray[1])
+    
+    let num1 = Double(userInputArray[0]) ?? 0.0
+    
+    let num2 = Double(userInputArray[2]) ?? 0.0
+    
+    let calcu = mathFunction(num1,num2)
+    
+    
+    return 0.0
+}
